5857301f6c76518d41b5cdc142a99c62f706745b
Tahmid L <103884318+tahlas@users.noreply.github.com>
2026-01-25 16:20:27 +0100

    Finished quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,41 +1,165 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <stdbool.h>
 
-#define ARRAY_SIZE 1000
+#define ARRAY_SIZE 10
+
+int partition(int array[], int low, int high);
 
 int array[ARRAY_SIZE];
 
-int main(){
-    for(int i = 0; i< ARRAY_SIZE; i++){
-        array[i] = rand() % 10000;
-    }
+double read_timer()
+{
+  static bool initialized = false;
+  static struct timeval start;
+  struct timeval end;
+  if (!initialized)
+  {
+    gettimeofday(&start, NULL);
+    initialized = true;
+  }
+  gettimeofday(&end, NULL);
+  return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
 
-    int middle = ARRAY_SIZE / 2;
-    int pivot = array[middle];
+//Needed to pass low and high indices to each thread
+typedef struct{
+    int low;
+    int high;   
+} ThreadArguments;
+
+bool isSorted(int array[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void *worker(void *arg)
+{
+    ThreadArguments *args = (ThreadArguments *)arg;
+
+    int low = args->low; 
+    int high = args->high;
+
+    
+    if(low < high){
+        int pivotIndex = partition(array, low, high);
+        pthread_t leftThread;
+        pthread_t rightThread;
+        ThreadArguments *leftArguments = malloc(sizeof(ThreadArguments));
+        ThreadArguments *rightArguments = malloc(sizeof(ThreadArguments));
+        leftArguments->low = low;
+        leftArguments->high = pivotIndex - 1;
+        rightArguments->low = pivotIndex + 1;
+        rightArguments->high = high;
+
+        pthread_create(&leftThread, NULL, worker, leftArguments);
+        pthread_create(&rightThread, NULL, worker, rightArguments);
+
+        pthread_join(leftThread, NULL);
+        pthread_join(rightThread, NULL);
+        free(leftArguments);
+        free(rightArguments);
+    }
+    
+    return NULL;
+}
+
+
+void initializeArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand() % 100;
+    }
+}
+
+void printArray(int array[], int size)
+{
+    printf("[");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("]\n");
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void initializeAttributes(pthread_attr_t *attr)
+{
+    pthread_attr_init(attr);
+    pthread_attr_setscope(attr, PTHREAD_SCOPE_SYSTEM);
+}
+
+int main()
+{
+    pthread_attr_t attr;
+    pthread_t thread;
+    
+    initializeAttributes(&attr);
+
+    initializeArray(array, ARRAY_SIZE);
+
+    #ifdef DEBUG
+    printf("Unsorted array:\n");
+    printArray(array, ARRAY_SIZE);
+    #endif
+
+    ThreadArguments* args = malloc(sizeof(ThreadArguments));
+    args->low = 0;
+    args->high = ARRAY_SIZE - 1;
+    double start_timer = read_timer();
+    pthread_create(&thread, &attr, worker, args);
+    pthread_join(thread, NULL);
+    free(args);
+    double end_timer = read_timer();
+
+    #ifdef DEBUG
+    printf("Sorted array:\n");
+    printArray(array, ARRAY_SIZE);
+    if(isSorted(array, ARRAY_SIZE)){
+        printf("Array is sorted.\n");
+    } else {
+        printf("Array is NOT sorted.\n");
+    }
+    #endif
+
+    printf("Time taken for sorting: %f seconds\n", end_timer - start_timer);
 
     return 0;
 }
 
-// void quicksort(int arr[], int low, int high){
-//     if (low < high){
-//         int pivot = partition(arr, low, high);
 
-//         quicksort(arr, low, pivot - 1);
-//         quicksort(arr, pivot + 1, high);
-//     }
-// }
+int partition(int array[], int low, int high)
+{
+    int pivot = array[high];
+    int i = (low - 1);
 
-// int partition(int arr[], int low, int high){
-//     int pivot = arr[high];
-//     int i = (low - 1);
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
 
-//     for (int j = low; j <= high - 1; j++){
-//         if (arr[j] < pivot){
-//             i++;
-//             swap(&arr[i], &arr[j]);
-//         }
-//     }
-//     swap(&arr[i + 1], &arr[high]);
-//     return (i + 1);
-// }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
