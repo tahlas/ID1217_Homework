e698a7c2cefdd8a8a81209cc9d58c948d07de2fa
Jonatan Tsehaye Hagos <jonth@kth.se>
2026-02-16 14:33:56 +0100

    HW4 v2

--- Homework4/HungryBirds.java
+++ Homework4/HungryBirds.java
@@ -1,101 +1,131 @@
 package ID1217_Homework.Homework4;
 
 public class HungryBirds {
-    
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        System.out.println("Starting the Hungry Birds simulation...");
+        int initialWorms = 5;
+        int dishCapacity = 5;
+        int numberOfBabyBirds = 3;
+        Dish.BabyBird[] babyBirds = new Dish.BabyBird[numberOfBabyBirds];
+        Dish dish = new Dish(initialWorms, dishCapacity);
+        Dish.ParentBird parentBird = dish.new ParentBird(dish);
+        for (int i = 0; i < babyBirds.length; i++) {
+            babyBirds[i] = dish.new BabyBird(dish);
+        }
+
+        parentBird.start();
+        for (Dish.BabyBird babyBird : babyBirds) {
+            babyBird.start();
+        }
     }
 }
-//monitor
-class Dish{
+
+// monitor
+class Dish {
     private int numberOfWorms;
     private int capacity;
-    Boolean notified = false;
-   
-    public Dish(int numberOfWorms, int capacity){
+    private boolean isBeingFilled = false;
+
+    public Dish(int numberOfWorms, int capacity) {
         this.capacity = capacity;
         this.numberOfWorms = numberOfWorms;
-    } 
+    }
 
-    public synchronized void eat(){
-
-        if(numberOfWorms > 0){
-            numberOfWorms--;
-            System.out.println("Baby bird eats a worm.");
-            if(numberOfWorms == 0){
-                System.out.println("Baby bird ate the last worm!");
-
-                //TODO: notify parent bird to fill the dish
-                if(notified){
-                    System.out.println("Parent bird is already notified to fill the dish.");
-                } else {
-                    System.out.println("Baby bird notifies the parent bird to fill the dish.");
-                    notified = true;
-                    notify();
-                }
-                notifyAll();
-            }  
-        }
-        else{
+    public synchronized void eat() {
+        while(numberOfWorms == 0 || isBeingFilled){
             System.out.println("Baby bird waits for the dish to be filled.");
-            wait();
+            try{
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        numberOfWorms--;
+        System.out.println("Baby bird ate a worm. Worms left: " + numberOfWorms);
+
+        if(numberOfWorms == 0){
+            System.out.println("Baby bird ate the last worm! Waking up the parent bird to fill the dish.");
+            notifyAll();
+        }
+    }    
+
+    public synchronized void fill() {
+        while(numberOfWorms > 0){
+            System.out.println("Parent bird waits for the dish to be empty.");
+            try{
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        isBeingFilled = true;
+        for(int i = 0; i < capacity; i++){
+            numberOfWorms++;
+            System.out.println("Parent bird adds a worm to the dish. Current number of worms: " + numberOfWorms);
+            try {
+                int oneSecond = 1000;
+                Thread.sleep(oneSecond); // Simulate time taken to add a worm
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        isBeingFilled = false;
+        System.out.println("Parent bird filled the dish. Waking up the baby birds.");
+        notifyAll();
+    }
+
+    class ParentBird extends Thread {
+
+        Dish dish;
+
+        public ParentBird(Dish dish) {
+            this.dish = dish;
+        }
+
+        public void run() {
+            while(true){
+                dish.fill();
+                try {
+                    int oneSecond = 1000;
+                    sleep(oneSecond); // Simulate time taken to fill the dish
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } 
+            }
         }
     }
 
-    public synchronized void fill(){
-        while(!isEmpty()){
-            System.out.println("Parent bird waits for the dish to be empty");
-            wait();
+    class BabyBird extends Thread {
+
+        Dish dish;
+
+        public BabyBird(Dish dish) {
+            this.dish = dish;
         }
-    }
 
-class ParentBird extends Thread{
-    public void run(){
-        while(true){
-
+        public void run(){
+            while(true){
+                dish.eat();
+                try {
+                    int oneSecond = 1000;
+                    sleep(oneSecond); // Simulate time taken to eat a worm
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } 
+            }
         }
-    }
 
-    Dish d;
-    public ParentBird(Dish d){
-        this.d = d;
+        // public void run(){
+        // while(true){
+        // synchronized(d){
+        // if(d.value == 1){
+        // d.value = 0;
+        // System.out.println("Baby bird eats the food.");
+        // d.notifyAll();
+        // }
+        // }
+        // }
+        // }
     }
-
-    public void run(){
-        
-    }
-
-    // public void run(){
-    //     while(true){
-    //         synchronized(d){
-    //             if(d.value == 0){
-    //                 d.value = 1;
-    //                 System.out.println("Parent bird fills the dish.");
-    //                 d.notifyAll();
-    //             }
-    //         }
-    //     }
-    // }
-}
-
-class BabyBird extends Thread{
-    public void run(){
-        
-    }
-    Dish d;
-    public BabyBird(Dish d){
-        this.d = d;
-    }
-
-    // public void run(){
-    //     while(true){
-    //         synchronized(d){
-    //             if(d.value == 1){
-    //                 d.value = 0;
-    //                 System.out.println("Baby bird eats the food.");
-    //                 d.notifyAll();
-    //             }
-    //         }
-    //     }
-    // }
 }
