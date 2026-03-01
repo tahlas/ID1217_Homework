57e75c42da50a224be8367178df0b351d25f3263
Jonatan Tsehaye Hagos <jonth@kth.se>
2026-03-02 00:08:40 +0100

    added comments

--- Homework5/ClientServer/Client.java
+++ Homework5/ClientServer/Client.java
@@ -7,15 +7,15 @@ public class Client {
     public static void main(String[] args) {
         int host = 1099;
         //String rmiregURL = "rmi//" + host;
-        String rmiregURL = "rmi://localhost:1099/teacher";
+        String rmiregURL = "rmi://localhost:1099/teacher"; // Construct the RMI registry URL to look up the teacher object
         try {
-            Teacher teacher = (Teacher) Naming.lookup(rmiregURL);
-            int index = Integer.parseInt(args[0]);
-            Student student = new StudentImpl(index);
-            teacher.registerStudent(student);
+            Teacher teacher = (Teacher) Naming.lookup(rmiregURL); 
+            int index = Integer.parseInt(args[0]);  
+            Student student = new StudentImpl(index); 
+            teacher.registerStudent(student); 
 
-            Student partner = teacher.getPartner(student);
-            System.out.println("Student " + student.getIndex() + " is paired with Student " + partner.getIndex());
+            Student partner = teacher.getPartner(student); // Get the partner assigned by the teacher
+            System.out.println("Student " + student.getIndex() + " is paired with Student " + partner.getIndex()); // Print the partner's index
 
         } catch (Exception e) {
             e.printStackTrace();
