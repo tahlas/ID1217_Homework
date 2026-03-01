da04e7278651c44a8a9c16a887cc6c3e76e6e1c5
Tahmid L <103884318+tahlas@users.noreply.github.com>
2026-03-01 11:45:43 +0100

    chore: removed unsolved problem

--- Homework5/Peer2Peer/Student.java
+++
@@ -1,9 +0,0 @@
-package Peer2Peer;
-
-import java.rmi.*;
-
-public interface Student extends Remote{
-    void setPartnerIndex(int partnerIndex) throws RemoteException;
-    int getIndex() throws RemoteException;
-    int getPartnerIndex() throws RemoteException;
-}
--- Homework5/Peer2Peer/Teacher.java
+++
@@ -1,7 +0,0 @@
-package Peer2Peer;
-
-public class Teacher {
-    public static void main(String[] args){
-        
-    }
-}
